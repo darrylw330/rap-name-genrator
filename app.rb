a9ef499af56dbc4820483992d1c626de9a94995d
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-26 16:28:55 +0000

    moved app from views, updated adjectives

--- app.rb
+++ app.rb
@@ -2,18 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 def generate_rap_name(name, last_purchase)
-  adjectives = ['Lil', 'Big', 'Young', 'Savage', 'Crazy', 'Fresh', 'King', 'Queen', 'G', 'Baby', 'Super', 'Red', 'Blue', 'Black', 'White']
+  adjectives = ['Lil', 'Big', 'Young', 'Savage', 'Crazy', 'Fresh', 'King', 'Queen', 'G', 'Baby', 'Super', 'Red', 'Blue', 'Black', 'White', 'Wavy', 'Wild', 'Lazy', 'Hazy', 'French', 'Spicy', 'Purple']
 
   adjective = adjectives.sample
 
   "#{adjective} #{last_purchase.capitalize}"
 end
 
-get '/rap_name' do
+get '/' do
   erb :form
 end
 
-post '/rap_name' do
+post '/' do
   name = params[:name]
   last_purchase = params[:last_purchase]
   rap_name = generate_rap_name(name, last_purchase)
