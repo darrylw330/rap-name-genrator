40d011f1e1b761b094379cc13423fbf9e7776a4f
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-26 16:36:16 +0000

    updated adj

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 def generate_rap_name(name, last_purchase)
-  adjectives = ['Lil', 'Big', 'Young', 'Savage', 'Crazy', 'Fresh', 'King', 'Queen', 'G', 'Baby', 'Super', 'Red', 'Blue', 'Black', 'White', 'Wavy', 'Wild', 'Lazy', 'Hazy', 'French', 'Spicy', 'Purple']
+  adjectives = ['Lil', 'Big', 'Young', 'Savage', 'Crazy', 'Fresh', 'King', 'Queen', 'G', 'Baby', 'Super', 'Red', 'Blue', 'Black', 'White', 'Wavy', 'Wild', 'Lazy', 'Hazy', 'French', 'Spicy', 'Purple', 'Pink', 'Yellow', 'Loud', 'Heavy', 'Bad', 'Gentle', 'Soft']
 
   adjective = adjectives.sample
 
@@ -17,5 +17,5 @@ post '/' do
   name = params[:name]
   last_purchase = params[:last_purchase]
   rap_name = generate_rap_name(name, last_purchase)
-  "Your rap name is: #{rap_name}"
+  "Your rap name is #{name} aka: #{rap_name}"
 end
