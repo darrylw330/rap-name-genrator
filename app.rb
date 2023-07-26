43f9a38242b4a58d4fff21737adb4db2ace909fd
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-26 16:09:23 +0000

    updated app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+def generate_rap_name(name, last_purchase)
+  adjectives = ['Lil', 'Big', 'Young', 'Savage', 'Crazy', 'Fresh', 'King', 'Queen', 'G', 'Baby', 'Super', 'Red', 'Blue', 'Black', 'White']
+
+  adjective = adjectives.sample
+
+  "#{adjective} #{last_purchase.capitalize}"
+end
+
+get '/rap_name' do
+  erb :form
+end
+
+post '/rap_name' do
+  name = params[:name]
+  last_purchase = params[:last_purchase]
+  rap_name = generate_rap_name(name, last_purchase)
+  "Your rap name is: #{rap_name}"
 end
